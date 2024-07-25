5487e9475331f36bc4b30927526f2707b45c1a7a
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-07-26 01:15:53 +0300

    before context

---
+++ src/NewPost.js
@@ -0,0 +1,34 @@
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+          autoComplete="off"
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postTitle"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default NewPost;
